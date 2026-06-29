d476c566c3072bd49a5d970f18b42498d6c4034d
Youness Hachimi <yhachimi@c1r4p8.1337.ma>
2026-06-29 16:29:40 +0100

    build:  animated_output  with hackers team

--- src/__main__.py
+++ src/__main__.py
@@ -1,17 +1,25 @@
 RED = '\033[0;31m'
 RESET = '\033[1;37m'
-
+BLUE = "\033[34m"
+GREEN = "\033[92m"
 try:
     import argparse as arg
     import json
-
+    from typing import Any
     import llm_sdk.llm_sdk as llm_model
-
     from .dataclase import Init
     from .system_promt import system_promt_builde
-except Exception as e:
+    from time import sleep, perf_counter
+except (Exception) as e:
     print(f"⚠️ {RED}{e}{RESET}")
+except KeyboardInterrupt:
+              exit(0)
 
+def animated_output(text: str, Color):
+    for c in text:
+            print(f"{Color}{c}{RESET}", end="", flush=True)
+            sleep(0.02)
+    print()
 
 def json_reader(file: str) -> dict:
     with open(file, "r") as f:
@@ -26,17 +34,24 @@ def get_valid_vocab(vocab: dict[str, int]) -> list[int]:
     return valid_ids
 
 
-def get_valid_output(output_json: str) -> str:
+def get_valid_output(output_json: str) -> Any:
     start = output_json.index("{")
-    brackets = 0 
-    for c in output_json:
-        if c == "{":
-                  brackets += 1
-        if c == "}":
-                    brackets -= 1
+    brackets = 0
+    for i in range(len(output_json)):
+        if output_json[i] == "{":
+            brackets += 1
+        if output_json[i] == "}":
+            brackets -= 1
+        if brackets == 0:
+                break
+    if brackets == 0: 
+        return output_json[start: i + 1]
+    return None
 
 def get_next_id(logits: list[float], valid_ids: list[int]) -> int:
-              return max(valid_ids, key=lambda i: logits[i] if i < len(logits) else float("-inf"))
+    return max(valid_ids, key=lambda i: logits[i] if i < len(
+        logits) else float("-inf"))
+
 
 if __name__ == "__main__":
     try:
@@ -63,9 +78,10 @@ if __name__ == "__main__":
         vocab = json_reader(Model.get_path_to_vocab_file())
         valid_ids = get_valid_vocab(vocab)
         name = "{'name'"
-        print(len(valid_ids))
+        start  = perf_counter()
         for p in prompts:
             st = f"\nUSER: {p.prompt}"
+            animated_output(st, BLUE)
             gen_prompt = Model.encode(sys_prompt + st)
             gen_ids = gen_prompt[0].tolist()
             all_gen = []
@@ -74,7 +90,14 @@ if __name__ == "__main__":
                 logits = Model.get_logits_from_input_ids(gen_ids + all_gen)
                 next_id = get_next_id(logits, valid_ids)
                 all_gen.append(next_id)
-                json_output = Model.decode(all_gen)
-                print(text)
+                json_output = get_valid_output(Model.decode(all_gen))
+                if json_output:
+                    break
+            animated_output(f"Qwen3-0.6B: {json_output}", GREEN) 
+        end_time = perf_counter()
+        total = (end_time - start) / 60
+        print(f"Done in {total:.2f} minutes")
     except Exception as e:
         print(f"⚠️ {RED}{e}{RESET}")
+    except KeyboardInterrupt:
+                 exit(0)

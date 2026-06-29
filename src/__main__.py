54656a0842a9945ce0c990c15a667a9939c33321
Youness Hachimi <yhachimi@c1r4p8.1337.ma>
2026-06-29 17:09:23 +0100

    feat: ensure output directory exists and write JSON output to specified file path using pathlib

--- src/__main__.py
+++ src/__main__.py
@@ -5,25 +5,36 @@ GREEN = "\033[92m"
 try:
     import argparse as arg
     import json
+    from pathlib import Path
+    from time import perf_counter, sleep
     from typing import Any
+
     import llm_sdk.llm_sdk as llm_model
+
     from .dataclase import Init
     from .system_promt import system_promt_builde
-    from time import sleep, perf_counter
 except (Exception) as e:
     print(f"⚠️ {RED}{e}{RESET}")
 except KeyboardInterrupt:
-              exit(0)
+    exit(0)
+
 
 def animated_output(text: str, Color):
     for c in text:
-            print(f"{Color}{c}{RESET}", end="", flush=True)
-            sleep(0.02)
+        print(f"{Color}{c}{RESET}", end="", flush=True)
+        sleep(0.02)
     print()
+
 
 def json_reader(file: str) -> dict:
     with open(file, "r") as f:
         return json.load(f)
+
+
+def creat_folder(path: str):
+    p = Path(path)
+    dir = p.parent
+    dir.mkdir(parents=True, exist_ok=True)
 
 
 def get_valid_vocab(vocab: dict[str, int]) -> list[int]:
@@ -35,7 +46,7 @@ def get_valid_vocab(vocab: dict[str, int]) -> list[int]:
 
 
 def get_valid_output(output_json: str) -> Any:
-    start = output_json.index("{")
+    start = output_json.find("{")
     brackets = 0
     for i in range(len(output_json)):
         if output_json[i] == "{":
@@ -43,10 +54,9 @@ def get_valid_output(output_json: str) -> Any:
         if output_json[i] == "}":
             brackets -= 1
         if brackets == 0:
-                break
-    if brackets == 0: 
-        return output_json[start: i + 1]
+            return output_json[start:i + 1]
     return None
+
 
 def get_next_id(logits: list[float], valid_ids: list[int]) -> int:
     return max(valid_ids, key=lambda i: logits[i] if i < len(
@@ -73,12 +83,14 @@ if __name__ == "__main__":
         prompts = initializer.prompts_init(json_reader(args.input))
         functions_definition = initializer.defs_init(
             json_reader(args.functions_definition))
+        path_out = args.output
         sys_prompt = system_promt_builde(functions_definition)
         Model = llm_model.Small_LLM_Model(args.model)
         vocab = json_reader(Model.get_path_to_vocab_file())
         valid_ids = get_valid_vocab(vocab)
-        name = "{'name'"
-        start  = perf_counter()
+        name = '{"name":'
+        outputs: list[dict] = []
+        start = perf_counter()
         for p in prompts:
             st = f"\nUSER: {p.prompt}"
             animated_output(st, BLUE)
@@ -86,18 +98,25 @@ if __name__ == "__main__":
             gen_ids = gen_prompt[0].tolist()
             all_gen = []
             all_gen.extend(Model.encode(name)[0].tolist())
-            for i in range(50):
+            for i in range(100):
                 logits = Model.get_logits_from_input_ids(gen_ids + all_gen)
                 next_id = get_next_id(logits, valid_ids)
                 all_gen.append(next_id)
                 json_output = get_valid_output(Model.decode(all_gen))
                 if json_output:
                     break
-            animated_output(f"Qwen3-0.6B: {json_output}", GREEN) 
+            if json_output:
+                out = json.loads(json_output)
+            outputs.append(out)
+            animated_output(f"Qwen3-0.6B: {json_output}", GREEN)
         end_time = perf_counter()
         total = (end_time - start) / 60
+        creat_folder(args.output)
+        with open(args.output, "w") as file:
+            json.dump(outputs, file, indent=2)
+        print(f"Output Created in {args.output}")
         print(f"Done in {total:.2f} minutes")
     except Exception as e:
         print(f"⚠️ {RED}{e}{RESET}")
     except KeyboardInterrupt:
-                 exit(0)
+        exit(0)
